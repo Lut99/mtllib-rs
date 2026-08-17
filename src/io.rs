e7e4692c2fc35f5162c02d48edd4db4d4486847c
Lut99 <mullertim99@gmail.com>
2026-08-17 10:05:58 +0200

    Externalized tokenizer in https://github.com/Lut99/obj-tokenizer-rs

--- src/io.rs
+++ src/io.rs
@@ -7,7 +7,6 @@
 
 use std::collections::HashMap;
 use std::io::Read;
-use std::str::FromStr as _;
 
 use thiserror::Error;
 
@@ -19,179 +18,24 @@ use crate::{Color, IlluminationModel, Material, Mtl};
 pub enum Error {
     #[error("Expected the dissolve at position {i}")]
     DMissingDissolve { i: u64 },
-    #[error("Expected a floating-point number at position {i}")]
-    F64 { i: u64 },
-    #[error("Failed to read {:?} as valid floating-point number", String::from_utf8_lossy(&raw))]
-    F64Value { raw: Vec<u8>, err: std::num::ParseFloatError },
     #[error("Expected the model at position {i}")]
     IllumMissingModel { i: u64 },
-    #[error("Failed to read from reader")]
-    Read(#[source] std::io::Error),
     #[error("Expected at least 3 color channels, got {got}")]
     KxNotEnoughChannels { i: u64, got: u8 },
-    #[error("Expected a keyword at position {i}")]
-    Keyword { i: u64 },
     #[error("Expected the name of the material at position {i}")]
     NewmtlMissingName { i: u64 },
     #[error("Attempted to assign a property without stating a material at position {i}")]
     NoNewMtl { i: u64 },
     #[error("Expected the exponent at position {i}")]
     NsMissingExponent { i: u64 },
+    #[error("{0}")]
+    Tokenizer(#[from] obj_tokenizer::Error),
     #[error("Expected the transparency at position {i}")]
     TrMissingTransparency { i: u64 },
-    #[error("Expected an unsigned number at position {i}")]
-    U32 { i: u64 },
-    #[error("Failed to read {:?} as valid unsigned number", String::from_utf8_lossy(&raw))]
-    U32Value { raw: Vec<u8>, err: std::num::ParseIntError },
     #[error("Unknown illumination model {model}")]
     UnknownIlluminationModel { model: u32 },
     #[error("Unknown keyword {:?}", String::from_utf8_lossy(&keyword))]
     UnknownKeyword { keyword: Vec<u8> },
-}
-
-
-
-
-
-/***** PARSER *****/
-/// A wrapper around a reader that does what we want.
-struct Parser<R> {
-    reader: R,
-    i:      u64,
-}
-impl<R> Parser<R> {
-    const fn new(reader: R) -> Self { Self { reader, i: 1 } }
-}
-impl<R: Read> Parser<R> {
-    /// Gets anything off the stream.
-    ///
-    /// # Returns
-    /// The index of the next byte and the byte, or [`None`].
-    fn next(&mut self) -> Result<Option<(u64, u8)>, Error> {
-        let mut b: u8 = 0;
-        if self.reader.read(std::slice::from_mut(&mut b)).map_err(Error::Read)? > 0 {
-            let i: u64 = self.i;
-            self.i += 1;
-            Ok(Some((i, b)))
-        } else {
-            Ok(None)
-        }
-    }
-
-    /// Gets the next character off the stream.
-    ///
-    /// This ignores comments & whitespace up to the first other byte.
-    ///
-    /// # Returns
-    /// The byte or [`None`].
-    fn byte(&mut self) -> Result<Option<(u64, u8)>, Error> {
-        enum State {
-            Start,
-            Comment,
-        }
-
-        let mut state = State::Start;
-        while let Some((i, b)) = self.next()? {
-            match state {
-                State::Start if b == b' ' || b == b'\t' || b == b'\r' || b == b'\n' => continue,
-                State::Start if b == b'#' => state = State::Comment,
-                State::Start => return Ok(Some((i, b))),
-
-                State::Comment if b == b'\n' => state = State::Start,
-                State::Comment => continue,
-            }
-        }
-        Ok(None)
-    }
-
-    /// Gets a keyword off the stream.
-    ///
-    /// This ignores comments & whitespace up to the first other byte.
-    fn keyword(&mut self) -> Result<Option<Vec<u8>>, Error> {
-        // Get a first byte that's a keyword byte.
-        let (i, b): (u64, u8) = match self.byte()? {
-            Some(b) => b,
-            None => return Ok(None),
-        };
-        if (b < b'a' || b > b'z') && (b < b'A' || b > b'Z') {
-            return Err(Error::Keyword { i });
-        }
-
-        // Then only read keyword bytes
-        let mut keyword = vec![b];
-        while let Some((_, b)) = self.next()? {
-            if (b < b'a' || b > b'z') && (b < b'A' || b > b'Z') && b != b'_' {
-                return Ok(Some(keyword));
-            }
-            keyword.push(b);
-        }
-        Ok(Some(keyword))
-    }
-
-    /// Pop a single string off the stream.
-    ///
-    /// That is, the remainder of the line after a whitespace.
-    fn string(&mut self) -> Result<Option<String>, Error> {
-        // Get a first byte
-        let b: u8 = match self.byte()? {
-            Some((_, b)) => b,
-            None => return Ok(None),
-        };
-
-        // Read until it's a newline
-        let mut s = vec![b];
-        while let Some((_, b)) = self.next()? {
-            if b == b'\n' {
-                return Ok(Some(String::from_utf8_lossy(&s).trim().into()));
-            }
-            s.push(b);
-        }
-        Ok(Some(String::from_utf8_lossy(&s).trim().into()))
-    }
-
-    /// Pop a single (unsigned) integer number off the stream.
-    fn u32(&mut self) -> Result<Option<u32>, Error> {
-        // Get a first byte in the range
-        let (i, b): (u64, u8) = match self.byte()? {
-            Some(b) => b,
-            None => return Ok(None),
-        };
-        if b < b'0' || b > b'9' {
-            return Err(Error::U32 { i });
-        }
-
-        // Read until it's a newline
-        let mut raw = vec![b];
-        while let Some((_, b)) = self.next()? {
-            if b < b'0' || b > b'9' {
-                return Ok(Some(u32::from_str(String::from_utf8_lossy(&raw).trim()).map_err(|err| Error::U32Value { raw, err })?));
-            }
-            raw.push(b);
-        }
-        Ok(Some(u32::from_str(String::from_utf8_lossy(&raw).trim()).map_err(|err| Error::U32Value { raw, err })?))
-    }
-
-    /// Pop a single floating-point number off the stream.
-    fn f64(&mut self) -> Result<Option<f64>, Error> {
-        // Get a first byte in the range
-        let (i, b): (u64, u8) = match self.byte()? {
-            Some(b) => b,
-            None => return Ok(None),
-        };
-        if (b < b'0' || b > b'9') && b != b'.' {
-            return Err(Error::F64 { i });
-        }
-
-        // Read until it's a newline
-        let mut raw = vec![b];
-        while let Some((_, b)) = self.next()? {
-            if (b < b'0' || b > b'9') && b != b'.' {
-                return Ok(Some(f64::from_str(String::from_utf8_lossy(&raw).trim()).map_err(|err| Error::F64Value { raw, err })?));
-            }
-            raw.push(b);
-        }
-        Ok(Some(f64::from_str(String::from_utf8_lossy(&raw).trim()).map_err(|err| Error::F64Value { raw, err })?))
-    }
 }
 
 
@@ -213,7 +57,7 @@ impl Mtl {
     pub fn from_reader<R: Read>(reader: R) -> Result<Self, Error> {
         let mut mtls: HashMap<String, Material> = HashMap::new();
         let mut currentmtl: Option<String> = None;
-        let mut parser = Parser::new(reader);
+        let mut parser = obj_tokenizer::Tokenizer::new(reader);
         loop {
             // Get a keyword first
             let keyword = match parser.keyword()? {
@@ -359,5 +203,12 @@ mod tests {
             let handle = std::fs::File::open(&entry_path).unwrap_or_else(|err| panic!("Failed to open file {entry_path:?}: {err}"));
             let _mtl = Mtl::from_reader(handle).unwrap_or_else(|err| panic!("Failed to parse file {entry_path:?} as .mtl file: {err}"));
         }
+    }
+
+    #[test]
+    fn test_crlf() {
+        let file = b"newmtl Test\r\n";
+        let mtl = Mtl::from_reader(file.as_slice()).unwrap();
+        assert_eq!(mtl.mtls.keys().collect::<Vec<&String>>(), vec![&"Test".to_string()]);
     }
 }
